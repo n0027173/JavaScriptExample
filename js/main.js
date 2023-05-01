a93e68496661877ce6cb025d2e00a2c29685f53e
n0027173 <126703919+n0027173@users.noreply.github.com>
2023-05-01 15:21:35 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-
 createDocumentation.addEventListener("click", () => {
 
-  let policyholderName = document.getElementById("policyholderName").value;
-  let policyholderNumber = document.getElementById("policyNumber").value;
-  let callReason = document.getElementById("callReason").value;
+  // let policyholderName = document.getElementById("policyholderName").value;
+  // let policyholderNumber = document.getElementById("policyNumber").value;
+  // let callReason = document.getElementById("callReason").value;
+
+  let policyholderName = document.querySelector("#policyholderName").value;
+  let policyholderNumber = document.querySelector("#policyNumber").value;
+  let callReason = document.querySelector("#callReason").value;
 
   document.getElementById("comments").value = policyholderName + " / " + policyholderNumber + " called for a(n) " + callReason + "."
 
